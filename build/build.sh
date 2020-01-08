83c88b3e300548b13ea7e88149f05dd1090b7a72
Cameron McQuinn <cameron.mcquinn@gmail.com>
2020-01-07 23:23:19 -0700

    Download wl18xx firmware before running Docker

--- build/build.sh
+++ build/build.sh
@@ -5,6 +5,16 @@ set -x # echo commands
 docker_image=cmcquinn/snickerdoodle-docker:kernel
 project=snickerdoodle-linux
 
+# get wl18xx firmware binaries
+projectdir=${PWD}
+cd ..
+git clone http://git.ti.com/git/wilink8-wlan/wl18xx_fw.git
+git clone https://github.com/krtkl/wlconf.git
+mkdir -p ${projectdir}/firmware/ti-connectivity
+cp wlconf/wl18xx-conf-default.bin ${projectdir}/firmware/ti-connectivity/wl18xx-conf.bin
+cp wl18xx_fw/wl18xx-fw-4.bin ${projectdir}/firmware/ti-connectivity/wl18xx-fw-4.bin
+cd ${projectdir}
+
 docker pull ${docker_image}
 docker rm ${project} &> /dev/null || true
 docker run --privileged --name ${project} -i \
