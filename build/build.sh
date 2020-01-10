cccba00bee311bb34bfb4e4f7fedbb2ed66b5607
Cameron McQuinn <cameron.mcquinn@gmail.com>
2020-01-10 14:19:05 -0700

    Get wl18xx firmware binaries from Bintray

--- build/build.sh
+++ build/build.sh
@@ -6,14 +6,11 @@ docker_image=cmcquinn/snickerdoodle-docker:kernel
 project=snickerdoodle-linux
 
 # get wl18xx firmware binaries
-projectdir=${PWD}
-cd ..
-git clone http://git.ti.com/git/wilink8-wlan/wl18xx_fw.git
-git clone https://github.com/krtkl/wlconf.git
-mkdir -p ${projectdir}/firmware/ti-connectivity
-cp wlconf/wl18xx-conf-default.bin ${projectdir}/firmware/ti-connectivity/wl18xx-conf.bin
-cp wl18xx_fw/wl18xx-fw-4.bin ${projectdir}/firmware/ti-connectivity/wl18xx-fw-4.bin
-cd ${projectdir}
+mkdir -p firmware/ti-connectivity
+curl -O https://dl.bintray.com/cmcquinn/snickerdoodle-wilink/wilink8fw.tar.gz
+tar xf wilink8fw.tar.gz
+mv wl18xx-fw-4.bin wl18xx-conf.bin wl1271-nvs.bin firmware/ti-connectivity
+rm wilink8fw.tar.gz
 
 docker pull ${docker_image}
 docker rm ${project} &> /dev/null || true
