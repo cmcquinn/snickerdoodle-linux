a01152ccc58830a35214b32acc72508adf8a5145
Cameron McQuinn <cameron.mcquinn@gmail.com>
2020-01-24 18:20:27 -0700

    build: use ccache

--- build/build.sh
+++ build/build.sh
@@ -8,9 +8,7 @@ project=snickerdoodle-linux
 docker pull ${docker_image}
 docker rm ${project} &> /dev/null || true
 docker run --privileged --name ${project} -i \
-     -v "${PWD}:/${project}" ${docker_image} \
+     -v "${PWD}:/${project}" \
+     -v "${HOME}/.ccache:/ccache" -e CCACHE_DIR=/ccache \
+     ${docker_image} \
      /bin/bash -c "cd ${project}; ./build/Recipe"
-
-# git clone https://github.com/cmcquinn/python-utils.git
-# pip3 install requests
-# python-utils/bintray.py cmcquinn replicookie-rootfs debian-buster ${workdir}/rootfs.tar.xz
